6c5d6af5b6fe1cc785f9b4e405b4e781dfd6fd4e
JiahuiZhangNCSU <168721445+JiahuiZhangNCSU@users.noreply.github.com>
2024-05-02 16:50:31 +0800

    Add files via upload

---
+++ README/README.txt
@@ -0,0 +1,10 @@
+CAvity Investigation Navigator (CAIN) uses alpha space to investigate the cavity of functional proteins.
+
+To install:
+cd CAIN/
+python setup.py
+To run demo:
+cd DEMO/
+python RunAll.py
+
+Detailed information will come up soon.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CAIN",
+    version="0.0.0",
+    description="CAvity investigation Navigator, a tool for functional protein cavity analysis.",
+    packages=find_packages(include=["CAIN*"]),
+    author="Jiahui Zhang",
+    author_email="jzhang71@ncsu.edu",
+    install_requires=[
+        'scipy',
+        'numpy',
+        'pandas',
+        'scikit-learn',
+    ]
+)
