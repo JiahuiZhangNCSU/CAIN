6c2a94aaabddcefd90a711650ea9582829322b77
JiahuiZhangNCSU <168721445+JiahuiZhangNCSU@users.noreply.github.com>
2024-05-06 16:46:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CAIN",
-    version="0.0.0",
+    version="0.1.0",
     description="CAvity investigation Navigator, a tool for functional protein cavity analysis.",
     packages=find_packages(include=["CAIN*"]),
     author="Jiahui Zhang",
